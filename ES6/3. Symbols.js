2819a3868fe9924a9bff60ea9e810536198b8024
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2017-11-30 20:36:09 +0700

    Move to note-code repo

--- ES6/3. Symbols.js
+++
@@ -1,9 +0,0 @@
-let symbol = Symbol('debug');
-let anotherSymbol = Symbol('debug');
-symbol == anotherSymbol // False, It have UNIQUE ID
-
-let symboil1 = Symbol.for('age');
-let symboil2 = Symbol.for('age');
-symboi1 == symbol2 // True
-
-// Well-known symbol
