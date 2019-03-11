7400b1f29c0361cc9aed215354c13a52845909a9
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2019-03-11 17:16:37 +0700

    Create CSharp.cs

---
+++ CSharp.cs
@@ -0,0 +1,24 @@
+```cs
+private T GetDefaultValue<T>(object obj)
+{
+    if (obj == null || string.IsNullOrEmpty(obj.ToString()))
+    {
+        return default(T);
+    }
+    return (T)Convert.ChangeType(obj.ToString(), typeof(T));
+}
+GetDefaultValue<bool>(dr["ProtectMaterial"]),
+
+var strObj = JsonConvert.SerializeObject(systemUserInfo);
+var obj = JsonConvert.DeserializeObject<SystemUserInfo>(aaa.ToString());
+
+foreach (dynamic item in data)
+{
+  fadas = item.SumOfHoursToComplete;
+}
+
+if (res != null && res.Data is var existed && existed != null)
+{
+    // do something
+}
+```
